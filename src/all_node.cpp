349f03f78251894264731dab3fe9d021cf15f7d2
Stew-000-1-0-011 <shnsk0204@gmail.com>
2023-04-16 22:19:22 +0900

    should never pass temporary object to addnode.

--- src/all_node.cpp
+++ src/all_node.cpp
@@ -1,3 +1,5 @@
+#include <iostream>
+
 #include <rclcpp/rclcpp.hpp>
 #include <nhk_video2/omni4_node.hpp>
 #include <nhk_video2/test_commander_node.hpp>
@@ -9,10 +11,16 @@ int main(int argc, char * argv[])
 	rclcpp::executors::MultiThreadedExecutor exec{};
 	rclcpp::NodeOptions options;
 
-	exec.add_node(std::make_shared<NhkVideo2::Omni4Node>(options));
-	exec.add_node(std::make_shared<NhkVideo2::TestCommanderNode>(options));
+	auto omni4 = std::make_shared<NhkVideo2::Omni4Node>(options);
+	auto test_commander = std::make_shared<NhkVideo2::TestCommanderNode>(options); 
+	exec.add_node(omni4);
+	exec.add_node(test_commander);
+	
+	std::cout << "tamaki started." << std::endl;
 
 	exec.spin();
+	
+	std::cout << "tamaki ended." << std::endl;
 
 	rclcpp::shutdown();
 
